f4af73aec29dc390acf1f3d9ed40ef40f0a0e6d0
Max Maus <max26@users.campus>
2020-01-29 16:53:50 -0600

    Added Angry Chicken and Rustlers Ribs

---
+++ Data/AngryChicken.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A class representing the Angry Chicken entree
+    /// </summary>
+    public class AngryChicken
+    {
+        private bool bread = true;
+        /// <summary>
+        /// Whether the Angry Chicken is served with bread
+        /// </summary>
+        public bool Bread
+        {
+            get { return bread;  }
+            set { bread = value; }
+        }
+
+        
+        /// <summary>
+        /// Whether the Angry Chicken contains pickles
+        /// </summary>
+        public bool Pickle { get; set; } = true; 
+
+        /// <summary>
+        /// The price of the chicken sandwich
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 5.99;
+            }
+        }
+
+        /// <summary>
+        /// The calories of the chicken sandwich
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 190;
+            }
+        }
+
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+
+                if (!bread) instructions.Add("hold bread");
+                if (!Pickle) instructions.Add("hold pickle");
+
+                return instructions;
+            }
+        }
+    }
+}
---
+++ Data/PecosPulledPork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A classs representing the Pecos Pulled Pork entree
+    /// </summary>
+    public class PecosPulledPork
+    {
+    }
+}
